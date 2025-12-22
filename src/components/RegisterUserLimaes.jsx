9dcb23388cff55c87626b1fa2eea0d6175e6ce09
muhammadirfan96 <muhammadirfanirfani808@gmail.com>
2025-12-22 11:47:55 +0800

    fix bug user auto admin

--- src/components/RegisterUserLimaes.jsx
+++ src/components/RegisterUserLimaes.jsx
@@ -3,7 +3,6 @@ import { axiosRT } from "../config/axios.js";
 import { useDispatch, useSelector } from "react-redux";
 import { setNotification } from "../redux/notificationSlice.js";
 import { setUserLimaes } from "../redux/userlimaesSlice.js";
-import { FaPencilAlt } from "react-icons/fa";
 
 const RegisterUserLimaes = () => {
   const dispatch = useDispatch();
@@ -42,6 +41,10 @@ const RegisterUserLimaes = () => {
     }
   };
 
+  useEffect(() => {
+    if (token && uid) findUserLimaes();
+  }, [token, uid]);
+
   const handleSubmit = (event) => {
     event.preventDefault();
     addUserLimaes();
@@ -57,7 +60,6 @@ const RegisterUserLimaes = () => {
           fullname,
           bagianlimaes_id,
           nomor_hp,
-          createdBy: uid,
         },
       );
       dispatch(
@@ -70,10 +72,7 @@ const RegisterUserLimaes = () => {
       setNip("");
       setFullname("");
       setBagianLimaes_id("");
-      setBagianLimaes([]);
-      setKeyBagianLimaes("");
-      setInputBagianLimaes(true);
-      setNamaBagianLimaes("");
+      setNomorHp("");
 
       dispatch(setUserLimaes(response.data));
     } catch (e) {
@@ -84,66 +83,16 @@ const RegisterUserLimaes = () => {
     }
   };
 
-  const uploadPicture = async (file) => {
-    try {
-      const formData = new FormData();
-      formData.append("picture", file);
-      await axiosInterceptors.patch(
-        `/${import.meta.env.VITE_APP_NAME}/${import.meta.env.VITE_APP_VERSION}/user-limaes/${userlimaes.id}/upload-picture`,
-        formData,
-      );
-      dispatch(
-        setNotification({
-          message: "selected data has been updated",
-          background: "bg-teal-100",
-        }),
-      );
-      findUserLimaes();
-    } catch (e) {
-      const arrError = e.response.data.error.split(",");
-      dispatch(
-        setNotification({ message: arrError, background: "bg-red-100" }),
-      );
-    }
-  };
-
-  const uploadTtd = async (file) => {
-    try {
-      const formData = new FormData();
-      formData.append("picture", file);
-      await axiosInterceptors.patch(
-        `/${import.meta.env.VITE_APP_NAME}/${import.meta.env.VITE_APP_VERSION}/user-limaes/${userlimaes.id}/upload-picture`,
-        formData,
-      );
-      dispatch(
-        setNotification({
-          message: "selected data has been updated",
-          background: "bg-teal-100",
-        }),
-      );
-      findUserLimaes();
-    } catch (e) {
-      const arrError = e.response.data.error.split(",");
-      dispatch(
-        setNotification({ message: arrError, background: "bg-red-100" }),
-      );
-    }
-  };
-
-  useEffect(() => {
-    if (token && uid) findUserLimaes();
-  }, [token, uid]);
-
-  // bagianlimaes_id
-  // option select
-
   const [bagianlimaes, setBagianLimaes] = useState([]);
-  const [keyBagianLimaes, setKeyBagianLimaes] = useState("");
 
-  const findBagianLimaes = async () => {
+  // filter
+  const [unitFilter, setUnitFilter] = useState("");
+  const [areaFilter, setAreaFilter] = useState("");
+
+  const findBagian = async () => {
     try {
       const response = await axiosInterceptors.get(
-        `/${import.meta.env.VITE_APP_NAME}/${import.meta.env.VITE_APP_VERSION}/bagian-limaes?area=${keyBagianLimaes}`,
+        `/${import.meta.env.VITE_APP_NAME}/${import.meta.env.VITE_APP_VERSION}/bagian-limaes?unit=${unitFilter}&area=${areaFilter}&limit=10000`,
       );
       setBagianLimaes(response.data.data);
     } catch (e) {
@@ -157,21 +106,12 @@ const RegisterUserLimaes = () => {
   };
 
   useEffect(() => {
-    findBagianLimaes();
-  }, [keyBagianLimaes]);
-
-  // input
-  const [inputBagianLimaes, setInputBagianLimaes] = useState(true);
-  const [namaBagianLimaes, setNamaBagianLimaes] = useState("");
-  const handleChangeOptionSelectBagianLimaes = (event) => {
-    const selected = event.target[event.target.selectedIndex];
-    setBagianLimaes_id(selected.value);
-    setInputBagianLimaes(true);
-    setNamaBagianLimaes(selected.getAttribute("data-additional-info"));
-  };
+    findBagian();
+  }, [unitFilter, areaFilter]);
 
   return (
     token &&
+    uid &&
     !userlimaes && (
       <>
         <div className="fixed inset-0 z-10 flex items-center justify-center bg-slate-900 bg-opacity-80">
@@ -180,12 +120,6 @@ const RegisterUserLimaes = () => {
             <p className="mb-4 border-b border-teal-700 pb-2 text-center text-base font-semibold text-teal-700">
               Register User Limaes
             </p>
-            {/* <button
-            onClick={closeModal}
-            className="absolute -right-2 -top-2 rounded-full bg-red-600 px-2 py-1 text-sm text-white shadow hover:bg-red-700"
-          >
-            ✕
-          </button> */}
 
             {/* Error */}
             {errForm && (
@@ -216,51 +150,61 @@ const RegisterUserLimaes = () => {
                   value={fullname}
                   onChange={(e) => setFullname(e.target.value)}
                 />
+                <div className="flex">
+                  {/* Unit Filter */}
+                  <select
+                    value={unitFilter}
+                    onChange={(e) => setUnitFilter(e.target.value)}
+                    className="w-full rounded-md border border-slate-300 p-2 text-sm focus:border-teal-500 focus:ring focus:ring-teal-200"
+                  >
+                    <option value="">Select Unit</option>
+                    {[...new Set(bagianlimaes.map((item) => item.unit))].map(
+                      (each, i) => (
+                        <option key={i} value={each}>
+                          {each}
+                        </option>
+                      ),
+                    )}
+                  </select>
+
+                  {/* Area Filter */}
+                  <select
+                    value={areaFilter}
+                    onChange={(e) => setAreaFilter(e.target.value)}
+                    className="w-full rounded-md border border-slate-300 p-2 text-sm focus:border-teal-500 focus:ring focus:ring-teal-200"
+                  >
+                    <option value="">Select Area</option>
+                    {[...new Set(bagianlimaes.map((item) => item.area))].map(
+                      (each, i) => (
+                        <option key={i} value={each}>
+                          {each}
+                        </option>
+                      ),
+                    )}
+                  </select>
+                </div>
 
                 {/* Bagian Limaes */}
-                {inputBagianLimaes ? (
-                  <button
-                    type="button"
-                    className="w-full rounded-md border border-slate-300 p-2 text-start text-sm"
-                    onClick={() => setInputBagianLimaes(false)}
-                  >
-                    {namaBagianLimaes ? (
-                      namaBagianLimaes
-                    ) : (
-                      <span className="text-slate-400">bagianlimaes...</span>
-                    )}
-                  </button>
-                ) : (
+                {unitFilter && areaFilter && (
                   <div className="flex gap-2">
                     <select
                       value={bagianlimaes_id}
-                      onChange={handleChangeOptionSelectBagianLimaes}
-                      className="w-1/2 rounded-md border border-slate-300 p-2 text-sm focus:border-teal-500 focus:ring focus:ring-teal-200"
+                      onChange={(e) => setBagianLimaes_id(e.target.value)}
+                      className="w-full rounded-md border border-slate-300 p-2 text-sm focus:border-teal-500 focus:ring focus:ring-teal-200"
                     >
                       <option value="">list bagianlimaes...</option>
                       {bagianlimaes.map((each) => (
-                        <option
-                          key={each._id}
-                          value={each._id}
-                          data-additional-info={`${each.unit}-${each.area}`}
-                        >
-                          {`${each.unit}-${each.area}`}
+                        <option key={each._id} value={each._id}>
+                          {`${each.unit}-${each.area}-${each.jabatan}`}
                         </option>
                       ))}
                     </select>
-                    <input
-                      type="text"
-                      placeholder="Search bagianlimaes"
-                      className="w-1/2 rounded-md border border-slate-300 p-2 text-sm focus:border-teal-500 focus:ring focus:ring-teal-200"
-                      value={keyBagianLimaes}
-                      onChange={(e) => setKeyBagianLimaes(e.target.value)}
-                    />
                   </div>
                 )}
 
                 {/* Nomor HP */}
                 <input
-                  type="number"
+                  type="text"
                   placeholder="Nomor HP"
                   className="w-full rounded-md border border-slate-300 p-2 text-sm focus:border-teal-500 focus:ring focus:ring-teal-200"
                   value={nomor_hp}
